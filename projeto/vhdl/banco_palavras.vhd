583e4969628c27119a25f72462c43c49ac47ed60
jv <jv>
2025-05-26 23:05:28 -0300

    [banco de palavras] entidade principal

--- projeto/vhdl/banco_palavras.vhd
+++ projeto/vhdl/banco_palavras.vhd
@@ -9,11 +9,36 @@ entity banco_palavras is
         en : in std_logic;
         lvl : in std_logic_vector(1 downto 0);
         binary_tip : out std_logic_vector(127 downto 0);
-        binary_word : out std_logic_vector(127 downto 0)    
+        binary_word : out std_logic_vector(127 downto 0)
     );
 end entity;
 
 architecture arch_banco_palavras of banco_palavras is
+
+component banco_lvl1 is
+	port(
+        binary_select: in std_logic_vector(6 downto 0);
+        binary_tip : out std_logic_vector(127 downto 0);
+        binary_word : out std_logic_vector(127 downto 0)
+    );
+end component;
+
+component banco_lvl2 is
+	port(
+        binary_select: in std_logic_vector(6 downto 0);
+        binary_tip : out std_logic_vector(127 downto 0);
+        binary_word : out std_logic_vector(127 downto 0)
+    );
+end component;
+
+component banco_lvl3 is
+	port(
+        binary_select: in std_logic_vector(5 downto 0);
+        binary_tip : out std_logic_vector(127 downto 0);
+        binary_word : out std_logic_vector(127 downto 0)
+    );
+end component;
+
 component pseudo_rand is
 	generic(
         number_range: natural := 4
@@ -28,6 +53,15 @@ signal lvl1_word_index : std_logic_vector(6 downto 0);
 signal lvl2_word_index : std_logic_vector(6 downto 0);
 signal lvl3_word_index : std_logic_vector(5 downto 0);
 
+signal word_lvl1 : std_logic_vector(127 downto 0);
+signal tip_lvl1 : std_logic_vector(127 downto 0);
+
+signal word_lvl2 : std_logic_vector(127 downto 0);
+signal tip_lvl2 : std_logic_vector(127 downto 0);
+
+signal word_lvl3 : std_logic_vector(127 downto 0);
+signal tip_lvl3 : std_logic_vector(127 downto 0);
+
 begin
     ---para escolher qual palavra usar----
     lvl1_rand_gen : pseudo_rand
@@ -40,5 +74,23 @@ begin
     generic map(6)
     port map(clock, en, lvl3_word_index);
     --------------------------------------
+    lvl1 : banco_lvl1
+    port map(lvl1_word_index, tip_lvl1, word_lvl1);
+    lvl2 : banco_lvl2
+    port map(lvl2_word_index, tip_lvl2, word_lvl2);
+    lvl3 : banco_lvl3
+    port map(lvl3_word_index, tip_lvl3, word_lvl3);
+
+    with lvl select binary_word <=
+        word_lvl1 when "01",
+        word_lvl2 when "10",
+        word_lvl3 when "11",
+        (others => '0') when others;
+
+    with lvl select binary_tip <=
+        tip_lvl1 when "01",
+        tip_lvl2 when "10",
+        tip_lvl3 when "11",
+        (others => '0') when others;
 
 end architecture;
